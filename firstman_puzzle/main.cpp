7609d3b8d484ef9690b290d0a33f6c2311839661
lineash <olol5454@naver.com>
2021-09-19 16:35:45 +0900

    Initial Commit

---
+++ firstman_puzzle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  firstman_puzzle
+//
+//  Created by line.ash on 2021/09/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
